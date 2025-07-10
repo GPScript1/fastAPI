73805365a48629d48ea6744a960758e3095c9708
xPrismatico <srfuentesavila@gmail.com>
2025-07-09 22:50:38 -0400

    feat: add schemas and endpoints

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,42 @@
+# app/api/routes.py
 from fastapi import APIRouter, HTTPException
-from app.schemas.request import PredictionRequest
-from app.services.predictor import predict_payment_time
+from app.schemas.request import PredictRequest
+from app.schemas.response import PredictResponse, ModelStatusResponse
+from app.services.predictor import predict_payment_time, train_model, get_model_status, delete_training_data
 
 router = APIRouter()
 
-@router.post("/predict", response_model=PredictionResponse)
-def predict(data: PredictionRequest):
-    prediction: predict_payment_time(data)
-    # Aquí iría la lógica de predicción
-    return {"prediction": "resultado"}
+@router.post("/predict", response_model=PredictResponse)
+def predict(data: PredictRequest):
+    try:
+        prediction = predict_payment_time(data)
+        return PredictResponse(dias_estimados=prediction)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/model/train")
+def train():
+    try:
+        result = train_model()
+        return {"message": "Modelo entrenado correctamente", "details": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/model/status", response_model=ModelStatusResponse)
+def status():
+    try:
+        status_info = get_model_status()
+        return status_info
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.delete("/training-data")
+def delete_data():
+    try:
+        delete_training_data()
+        return {"message": "Datos de entrenamiento eliminados"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
